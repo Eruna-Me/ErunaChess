ca552974a4266607562681d1bbaee4388d5829bb
Hidde Bartelink <hiddetelink@live.nl>
2018-11-01 17:25:13 +0100

    Fixed black pawn move generation

--- ErunaChess/Movegen/Movegenerator.cs
+++ ErunaChess/Movegen/Movegenerator.cs
@@ -54,46 +54,45 @@ namespace ErunaChess
 					}
 				}
 			}
-			// generate black pawn moves (wrong D:)
+			// generate black pawn moves
 			else
 			{
 				for (int i = 0; i < board.pieceCount[(int)Board.Pieces.blackPawn]; i++)
 				{
 					int square = board.pieces[(int)Board.Pieces.blackPawn, i];
-
 					// if 2nd rank, promotion fun.
-					if (square <= (int)Global.Square.H2)
+					if ( square <= (int)Global.Square.H2)
 					{
 						if (board.board[square - Global.boardWidth] == Global.empty)
-							AddMove.PromotionMove(board, Move.Write(square, square + Global.boardWidth, 0, 0, false, false, false), moveList);
+							AddMove.PromotionMove(board, Move.Write(square, square - Global.boardWidth, 0, 0, false, false, false), moveList);
 						//captures
 						if ((board.board[square - Global.boardWidth + 1] & Global.border) == enemy)
-							AddMove.PromotionMove(board, Move.Write(square, board.board[square + Global.boardWidth + 1], board.board[square - Global.boardWidth + 1], 0, false, false, false), moveList);
+							AddMove.PromotionMove(board, Move.Write(square, square - Global.boardWidth + 1, board.board[square - Global.boardWidth + 1], 0, false, false, false), moveList);
 
 						if ((board.board[square - Global.boardWidth - 1] & Global.border) == enemy)
-							AddMove.PromotionMove(board, Move.Write(square, board.board[square + Global.boardWidth - 1], board.board[square - Global.boardWidth - 1], 0, false, false, false), moveList);
+							AddMove.PromotionMove(board, Move.Write(square, square - Global.boardWidth - 1, board.board[square - Global.boardWidth - 1], 0, false, false, false), moveList);
 					}
 					else
 					{
 						if (board.board[square - Global.boardWidth] == Global.empty)
 						{
-							AddMove.QuietPawnMove(board, Move.Write(square, square + Global.boardWidth, 0, 0, false, false, false), moveList);
+							AddMove.QuietPawnMove(board, Move.Write(square, square - Global.boardWidth, 0, 0, false, false, false), moveList);
 
 							if (square >= (int)Global.Square.A7 && board.board[square - Global.boardWidth * 2] == Global.empty)
-								AddMove.QuietPawnMove(board, Move.Write(square, square + Global.boardWidth, 0, 0, false, true, false), moveList);
+								AddMove.QuietPawnMove(board, Move.Write(square, square - Global.boardWidth * 2, 0, 0, false, true, false), moveList);
 						}
 						//captures
 						if ((board.board[square - Global.boardWidth + 1] & Global.border) == enemy)
-							AddMove.PawnCaptureMove(board, Move.Write(square, board.board[square + Global.boardWidth + 1], board.board[square - Global.boardWidth + 1], 0, false, false, false), moveList);
+							AddMove.PawnCaptureMove(board, Move.Write(square, square - Global.boardWidth + 1, board.board[square - Global.boardWidth + 1], 0, false, false, false), moveList);
 
 						if ((board.board[square - Global.boardWidth - 1] & Global.border) == enemy)
-							AddMove.PawnCaptureMove(board, Move.Write(square, board.board[square + Global.boardWidth - 1], board.board[square - Global.boardWidth - 1], 0, false, false, false), moveList);
+							AddMove.PawnCaptureMove(board, Move.Write(square, square - Global.boardWidth - 1, board.board[square - Global.boardWidth - 1], 0, false, false, false), moveList);
 						//enpassant
-						if (board.board[square - Global.boardWidth + 1] == board.enpassantSquare)
-							AddMove.EnpassantMove(board, Move.Write(square, board.board[square + Global.boardWidth + 1], board.board[square - Global.boardWidth + 1], 0, true, false, false), moveList);
+						if (square - Global.boardWidth + 1 == board.enpassantSquare)
+							AddMove.EnpassantMove(board, Move.Write(square, board.enpassantSquare, board.board[board.enpassantSquare], 0, true, false, false), moveList);
 
-						if (board.board[square - Global.boardWidth - 1] == board.enpassantSquare)
-							AddMove.EnpassantMove(board, Move.Write(square, board.board[square + Global.boardWidth - 1], board.board[square - Global.boardWidth - 1], 0, true, false, false), moveList);
+						if (square - Global.boardWidth - 1 == board.enpassantSquare)
+							AddMove.EnpassantMove(board, Move.Write(square, board.enpassantSquare, board.board[board.enpassantSquare], 0, true, false, false), moveList);
 					}
 				}
 			}
--- ErunaChess/Program.cs
+++ ErunaChess/Program.cs
@@ -15,7 +15,8 @@ namespace ErunaChess
 			MoveList moveList = new MoveList();
 			//moveList.moves[0] = new Move(); 
 			//IO.ParseFen(board, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-			IO.ParseFen(board, "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1");
+			//IO.ParseFen(board, "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1");
+			IO.ParseFen(board, "rnbqkbnr/p1p1p3/3p3p/1p1p4/2P1Pp2/8/PP1P1PpP/RNBQKB1R b KQkq e3 0 1");
 			Console.WriteLine(board.pieceCount[(int)Board.Pieces.whitePawn]);
 			Board.Draw(board);
 			Console.WriteLine(board.castlePermission);
@@ -25,7 +26,6 @@ namespace ErunaChess
 			Debug.DrawAttackedSquares(board, Global.black);
 			MoveGenerator.GenerateAllMoves(board, moveList);
 			Console.ReadKey();
-		}
-	
+		}	
 	}
 }
