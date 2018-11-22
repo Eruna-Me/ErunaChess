c32668f6f8c18bd1a96ce0afedd53563e5b1f854
Hidde Bartelink <hiddetelink@live.nl>
2018-11-22 16:12:06 +0100

    Fixed makemove, hopefully

--- ErunaChess/Movegen/Move.cs
+++ ErunaChess/Movegen/Move.cs
@@ -22,9 +22,9 @@ namespace ErunaChess
 		public static int Captured(int piece) => (piece >> 16) & 0x3F;
 		public static int Promoted(int piece) => (piece >> 22) & 0x3F;
 
-		public static int EnPassantFlag() => 1 >> 28;
+		public static int EnPassantFlag() => 1 << 28;
 		public static int PawnStartFlag() => 1 << 29;
-		public static int CastleFlag() => 1 >> 30;
+		public static int CastleFlag() => 1 << 30;
 
 		public static int CaptureFlag() => 0x1F >> 16;
 		public static int PromotedFlag() => 0x1F >> 22;
